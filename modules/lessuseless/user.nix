d75c2f11719d1c5243fbf96d96c45a5b82543276
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-10-27 02:39:15 -0600

    Fix home-manager timeout conflict with lib.mkForce

--- modules/lessuseless/user.nix
+++ modules/lessuseless/user.nix
@@ -24,28 +24,30 @@ let
       services.displayManager.autoLogin.user = "lessuseless";
     };
 
-  linux = {
-    users.users.lessuseless = {
-      isNormalUser = true;
-      uid = 1000;  # Fixed UID for impermanence
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "video"      # Access to video devices
-        "audio"      # Access to audio devices
-        "plugdev"    # Access to pluggable devices (USB, etc.)
-      ];
-      # Password configured via initialHashedPassword
-      # Use initialHashedPassword with users.mutableUsers = false from impermanence
-      initialHashedPassword = "$6$.K.oyv.fU6Gqu1ov$aia2TQleZO1L7VhUS6XoBAf08ZXx7ATE42B6/l0G5YGrwbUT0eOZDTuSHAmAHW3L50mBVcyh3m3Fk7ndq9Jby/";
+  linux =
+    { lib, ... }:
+    {
+      users.users.lessuseless = {
+        isNormalUser = true;
+        uid = 1000;  # Fixed UID for impermanence
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+          "video"      # Access to video devices
+          "audio"      # Access to audio devices
+          "plugdev"    # Access to pluggable devices (USB, etc.)
+        ];
+        # Password configured via initialHashedPassword
+        # Use initialHashedPassword with users.mutableUsers = false from impermanence
+        initialHashedPassword = "$6$.K.oyv.fU6Gqu1ov$aia2TQleZO1L7VhUS6XoBAf08ZXx7ATE42B6/l0G5YGrwbUT0eOZDTuSHAmAHW3L50mBVcyh3m3Fk7ndq9Jby/";
+      };
+
+      # Require password for sudo
+      security.sudo.wheelNeedsPassword = true;
+
+      # Increase timeout for home-manager activation (Doom Emacs can take a while)
+      systemd.services."home-manager-lessuseless".serviceConfig.TimeoutStartSec = lib.mkForce "10min";
     };
-
-    # Require password for sudo
-    security.sudo.wheelNeedsPassword = true;
-
-    # Increase timeout for home-manager activation (Doom Emacs can take a while)
-    systemd.services."home-manager-lessuseless".serviceConfig.TimeoutStartSec = "10min";
-  };
 
   darwin.system.primaryUser = "lessuseless";
 
